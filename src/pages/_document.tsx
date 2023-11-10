d13e17cef4718aede8dd036382c695c658d23f91
Emanuel Boaventura <emanuelbmatos321@gmail.com>
2023-11-09 21:43:45 -0400

    feat: configure base project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,21 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='anonymous'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;200;300;400;500;600;700;800&display=swap'
+          rel='stylesheet'
+        />
+
+        <link rel='icon' type='image/png' href='/favicon.png' />
+      </Head>
       <body>
         <Main />
         <NextScript />
