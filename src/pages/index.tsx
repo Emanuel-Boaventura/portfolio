ae722b4595d02e6745caeb5aa4d28c648ca8e539
Emanuel Boaventura <emanuelbmatos321@gmail.com>
2024-02-15 21:44:03 -0400

    feat: add menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,22 @@ import { About } from '@/components/About'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
+import { Menu } from '@/components/Menu'
+import { useState } from 'react'
 
 export default function Home() {
+  const [open, setOpen] = useState(false)
+  console.log('open:', open)
+
   return (
-    <div className='min-h-screen bg-[#0d0d0d] text-white flex flex-col'>
-      <Header />
+    <div className='min-h-screen bg-[#0d0d0d] text-white flex flex-col relative overflow-hidden'>
+      <Header setOpen={setOpen} />
+      <Menu open={open} setOpen={setOpen} />
       <main className='container mx-auto'>
         <Hero />
-        <About />
+        {/* <About /> */}
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
