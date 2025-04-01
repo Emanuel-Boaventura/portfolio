0b585e3c431a6b139a86dfd8fa36e68c08a30ac9
Emanuel-Boaventura <emanuelbm.dev@gmail.com>
2025-04-01 16:49:12 -0400

    feat: update career

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -1,66 +1,71 @@
 import useInViewport from '@/hooks/use-in-viewport'
 import { jobs } from '@/mock/jobs'
 import Image from 'next/image'
+import { Fragment } from 'react'
 
 export function Career() {
   const ref = useInViewport()
 
   function renderJobs() {
-    return jobs.map(job => (
-      <div
-        key={job.companyName}
-        className={`flex w-full flex-col items-center gap-4 bg-[rgba(20,_20,_20)] px-3 py-4 text-center shadow-lg min-[425px]:px-6 min-[425px]:text-left sm:max-w-[720px] ${
-          job.actualJob ? 'neon-shadow' : ''
-        }`}
-      >
-        <div className='flex flex-col gap-3 min-[425px]:w-full min-[425px]:flex-row min-[425px]:gap-6'>
-          <Image
-            src={job.companyLogo}
-            alt={`Logo ${job.companyName}`}
-            className='mx-auto h-auto w-20 min-[425px]:mx-0'
-          />
+    return jobs.map((job, idx) => (
+      <Fragment key={job.companyName}>
+        <div
+          className={`flex w-full flex-col items-center gap-4 bg-[rgba(20,_20,_20)] px-3 py-4 text-center shadow-lg min-[425px]:px-6 min-[425px]:text-left sm:max-w-[720px] ${
+            job.actualJob ? 'neon-shadow' : ''
+          }`}
+        >
+          <div className='flex flex-col gap-3 min-[425px]:w-full min-[425px]:flex-row min-[425px]:gap-6'>
+            <Image
+              src={job.companyLogo}
+              alt={`Logo ${job.companyName}`}
+              className='h-20 w-20 self-center min-[425px]:mx-0'
+            />
 
-          <div className='neon h-px w-full min-[425px]:h-auto min-[425px]:w-px' />
+            <div className='neon h-px w-full min-[425px]:h-auto min-[425px]:w-px' />
 
-          <div>
             <div>
-              <p className='text-sm sm:text-base'>Empresa:</p>
-              <p className='text-base text-lime-500 sm:text-lg'>
-                {job.companyName}
+              <div>
+                <p className='text-sm sm:text-base'>Empresa:</p>
+                <p className='text-base text-lime-500 sm:text-lg'>
+                  {job.companyName}
+                </p>
+              </div>
+
+              <div>
+                <p className='text-sm sm:text-base'>Período:</p>
+                <p className='text-base text-lime-500 sm:text-lg'>
+                  {job.duration.start} - {job.duration.finish}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col gap-3'>
+            <div>
+              <p className='text-sm sm:text-base'>Descrição:</p>
+              <p className='text-xs text-lime-500 sm:text-sm'>
+                {job.description}
               </p>
             </div>
 
             <div>
-              <p className='text-sm sm:text-base'>Período:</p>
-              <p className='text-base text-lime-500 sm:text-lg'>
-                {job.duration.start} - {job.duration.finish}
-              </p>
+              <p className='text-sm sm:text-base'>Tecnologias utilizadas:</p>
+              <div className='mx-auto mt-2 flex flex-wrap justify-center gap-6 min-[425px]:justify-normal lg:mx-0'>
+                {job.techs.map(tech => (
+                  <Image
+                    src={tech.img}
+                    key={tech.alt}
+                    alt={`Logo ${tech.alt}`}
+                    className='h-7 w-7 object-contain sm:h-8 sm:w-8'
+                  />
+                ))}
+              </div>
             </div>
           </div>
         </div>
-        <div className='flex flex-col gap-3'>
-          <div>
-            <p className='text-sm sm:text-base'>Descrição:</p>
-            <p className='text-xs text-lime-500 sm:text-sm'>
-              {job.description}
-            </p>
-          </div>
-
-          <div>
-            <p className='text-sm sm:text-base'>Tecnologias utilizadas:</p>
-            <div className='mx-auto mt-2 flex flex-wrap justify-center gap-6 min-[425px]:justify-normal lg:mx-0'>
-              {job.techs.map(tech => (
-                <Image
-                  src={tech.img}
-                  key={tech.alt}
-                  alt={`Logo ${tech.alt}`}
-                  className='h-7 w-7 object-contain sm:h-8 sm:w-8'
-                />
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
+        {idx !== jobs.length - 1 && (
+          <div className='neon -z-10 h-16 w-px overflow-hidden' />
+        )}
+      </Fragment>
     ))
   }
 
@@ -75,7 +80,7 @@ export function Career() {
           Carreira
         </h2>
 
-        {renderJobs()}
+        <div className='flex w-full flex-col items-center'>{renderJobs()}</div>
       </div>
     </section>
   )
--- src/mock/jobs.ts
+++ src/mock/jobs.ts
@@ -1,17 +1,18 @@
 import dompixel from '@/public/dompixel.svg'
+import trescon from '@/public/3con.png'
 import { StaticImageData } from 'next/image'
 import { ITech, techs } from './techs'
 
 interface IJobs {
   companyName: string
   companyLogo: StaticImageData
-  actualJob: true
   duration: {
     start: string
     finish: string
   }
   description: string
   techs: ITech[]
+  actualJob: boolean
 }
 
 const {
@@ -29,6 +30,18 @@ const {
 
 export const jobs: IJobs[] = [
   {
+    companyName: '3CON Consultoria e Sistemas',
+    companyLogo: trescon,
+    duration: {
+      start: 'jan/2025',
+      finish: 'Atualmente',
+    },
+    description:
+      'Atuo como Lead Mobile Developer responsável pelo desenvolvimento completo de aplicativos com React Native, trabalhando de forma autônoma na tomada de decisões técnicas e arquiteturais para os projetos. Além disso, participo como Dev Frontend em equipes multidisciplinares, contribuindo para o desenvolvimento e manutenção de uma plataforma interna utilizando React, assegurando qualidade e desempenho nas entregas.',
+    actualJob: true,
+    techs: [react, tailwind, typescript],
+  },
+  {
     companyName: 'DomPixel Tecnologia',
     companyLogo: dompixel,
     duration: {
@@ -37,7 +50,7 @@ export const jobs: IJobs[] = [
     },
     description:
       'Como Dev Full-Stack na DomPixel atuei na frente do desenvolvimento de aplicações do começo ao fim, entre elas: Plataformas de RH, sites de divulgação e gestão de cursos, plataforma para gestão de campanhas de doação e plataformas para controle de produtos e vendas. Participando em reuniões periódicas com o cliente e reuniões diárias com o time de desenvolvimento para tomada de decisão em relação aos projetos.',
-    actualJob: true,
+    actualJob: false,
     techs: [
       react,
       next,
